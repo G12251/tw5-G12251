3081f31fe47cc8598434f273a83812302be2cfe3
G12251 <wiut12251@gmail.com>
2021-10-19 15:05:34 +0500

    task6

---
+++ main.py
@@ -0,0 +1,27 @@
+#secretNumber = "16"
+#while True:
+#   if line == secretNumber:
+#        break
+#    print("False")
+#print('True!')
+# for x in range (5):
+#     print(x)
+
+# for x in range (10, 20, 2):
+#     print(x)
+
+# color_list = ["Red", "Green", "White", "Black"]
+# print (color_list [0])
+# print (color_list [-1])
+
+n=10
+for i in range(n):
+    for j in range(i):
+        print('% ', end="")
+    print('')
+
+n=1
+for i in range(n):
+    for j in range(i):
+        print('% ', end="")
+    print('')
